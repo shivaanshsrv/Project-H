bcdedda63b8baa2a98963ac0628f708d8a8c96c9
Shivansh🐱 <maybeshivansh@gmail.com>
2025-12-15 23:15:24 +0530

    Resolved MongoDB error :)

--- web/backend/src/config/db.js
+++ web/backend/src/config/db.js
@@ -3,12 +3,14 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            dbName: "ProjectH"
+            dbName: "ProjectH",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
         });
 
         console.log("✅ MongoDB Connected Successfully");
-    } catch (err) {
-        console.log("❌ MongoDB Error:", err.message);
+    } catch (error) {
+        console.error("❌ MongoDB Connection Error:", error.message);
         process.exit(1);
     }
 };
